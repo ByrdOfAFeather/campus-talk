6d1291bbed9343ab4c0e3b87033e995ea6c7638b
Brian <brianru@live.unc.edu>
2019-12-11 13:40:54 -0500

    Fixed stuff i think

--- public/js/briansAdditions.js
+++ public/js/briansAdditions.js
@@ -82,9 +82,14 @@ async function toggleLike(currentUserID, contentID) {   // if the logged in user
 async function editPost(userID, postID, newTitle, newBody) {
     let res = await axios.get(`http://localhost:3000/private/posts/${postID}/`, {headers: {"Authorization": "Bearer " + localStorage.getItem("apiKey")}});
 
+    let otherAuthor = res.data.result['author'];
+    console.log("before delete: " + otherAuthor);
+
+
     let deleteResult = await axios.delete(`http://localhost:3000/private/posts/${postID}`, {headers: {"Authorization": "Bearer " + localStorage.getItem("apiKey")}});
 
     let sameAuthor = res.data.result['author'];
+    console.log("after delete" + sameAuthor);
 
 
     await axios.post(`http://localhost:3000/private/posts/${postID}/`, {
